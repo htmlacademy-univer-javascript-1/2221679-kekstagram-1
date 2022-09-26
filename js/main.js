2ab8bfc584a3a18f45e05c1b0cdf89e2fc371f13
Evgeniy <zhenechka-makhov@mail.ru>
2022-09-26 17:54:42 +0500

    исправил ошибки и добавил функции

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 //функция случайных чисел в диапозоне от min к max.
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-  console.log (random());
+const random = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+random();
 //данную функцию взял с сайта : https://habr.com/ru/company/ruvds/blog/534108/
 
 
 //данную функцию создал сам
 
-let func2 = function(name, max_lenght) {
-    name = String(name);
-    length_name = name.length;
-    if(max_lenght >=length_name){
-        return true;
-    } else {
-        return false;
-    }
+const func2 = function(name, maxlenght) {
+  name = String(name);
+  const lengthname = name.length;
+  if(maxlenght >=lengthname){
+    return true;
+  } else {
+    return false;
+  }
 
-}
-console.log(func2());
+};
+func2();
